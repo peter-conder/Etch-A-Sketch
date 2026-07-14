7ae765fe5b5b9372605c31f34a509e3212c061c4
peter-conder <pconder2005@gmail.com>
2026-07-14 09:29:20 -0600

    add sqaures stay black on hover

--- script.js
+++ script.js
@@ -5,13 +5,12 @@
 
 const containerDiv = document.createElement('div');
 containerDiv.classList.add('containerDiv');
-//containerDiv.textContent = "hello there";
 document.body.appendChild(containerDiv);
 
 for (let i = 0; i < 16; i++) {
 const rowDiv = document.createElement('div');
 rowDiv.classList.add('rowDiv');
-//rowDiv.textContent = 'div';
+//rowDiv.classList.add("hovered");
 containerDiv.append(rowDiv);
 
     for (let i = 0; i < 16; i++) {
@@ -21,3 +20,22 @@ containerDiv.append(rowDiv);
         rowDiv.append(columnDiv);
     }
 }
+const squares = document.querySelectorAll('.columnDiv');
+squares.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+        square.classList.add('hovered');
+    });
+});
+columnDiv.classList.add("hovered");
+
+/*const upDiv = document.querySelector(".columnDiv");
+upDiv.classList.add('hovered');
+upDiv.addEventListener("mouseenter", () => {upDiv.classList.add("hovered");}
+);
+
+/**
+ * hover effect stays on after mouse moves on:
+ * best to use js + eventlisteners to add a class to a div once it is hovered over, then have that class modify that div using css.
+ * 1) add eventListener
+ * 2) eventListener adds class
+ */
