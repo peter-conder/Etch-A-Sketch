249d650f1b666d0b39a26ea99b1ff2bdade4d8c6
peter-conder <pconder2005@gmail.com>
2026-07-14 10:20:07 -0600

    add function drawCanvas

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-/** Etch-a-sketch
- * 
+/**
+ * So I have the basic thing done now.
+ * what needs to happen now is I need to have the 'drawing canvas' function activate whenever you choose a new canvas size.
+ * the drawCanvas function also needs to run once when the page is loaded with a size of 16x16, but be redrawn every time the button is clicked.
+ * finally, with large numbers, the divs don't quite line up on the x axis. This needs to be fixed,
  */
 
+
+let sizeInt = 16;
+
+function sizePrompt() {
+    let sizeString = prompt("How big do you want the canvas to be?")
+    sizeInt = parseInt(sizeString);
+    if (sizeInt > 100) {
+        alert("too large! select smaller number!");
+    }
+};
+
+//draws canvas
+function drawCanvas(size) {
+const promptButton = document.querySelector(".sizeButton");
+promptButton.addEventListener ("click", sizePrompt);
 
 const containerDiv = document.createElement('div');
 containerDiv.classList.add('containerDiv');
 document.body.appendChild(containerDiv);
-
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < size; i++) {
 const rowDiv = document.createElement('div');
 rowDiv.classList.add('rowDiv');
 //rowDiv.classList.add("hovered");
 containerDiv.append(rowDiv);
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < size; i++) {
         const columnDiv = document.createElement('div');
         columnDiv.classList.add('columnDiv');
         //columnDiv.textContent = 'vid';
         rowDiv.append(columnDiv);
     }
 }
+}
+
+drawCanvas(16);
+
+//hover
 const squares = document.querySelectorAll('.columnDiv');
 squares.forEach((square) => {
     square.addEventListener("mouseenter", () => {
@@ -28,14 +50,3 @@ squares.forEach((square) => {
 });
 columnDiv.classList.add("hovered");
 
-/*const upDiv = document.querySelector(".columnDiv");
-upDiv.classList.add('hovered');
-upDiv.addEventListener("mouseenter", () => {upDiv.classList.add("hovered");}
-);
-
-/**
- * hover effect stays on after mouse moves on:
- * best to use js + eventlisteners to add a class to a div once it is hovered over, then have that class modify that div using css.
- * 1) add eventListener
- * 2) eventListener adds class
- */
